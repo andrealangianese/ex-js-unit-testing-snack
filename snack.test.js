202c5345005fc68d704fa545fc2920fed3ea3f28
andrealangianese <andre.langia.01@gmail.com>
2026-05-12 09:23:24 +0200

    copio snack

---
+++ snack.test.js
@@ -0,0 +1,35 @@
+// snack 1 
+
+test('La funzione getInitials restituisce le iniziali di un nome completo.', () => {
+
+})
+// snack 2
+
+test('La funzione createSlug restituisce una stringa in lowercase', () => {
+
+})
+// snack 3 
+
+test('La funzione average calcola la media aritmetica di un array di numeri.', () => {
+
+})
+// snack 4 
+
+test('La funzione createSlug sostituisce gli spazi con -', () => {
+
+})
+// snack 5 
+
+test('La funzione isPalindrome verifica se una stringa è un palindromo."', () => {
+
+})
+// snack 6 
+
+test('La funzione createSlug lancia un errore se il titolo è vuoto o non valido.', () => {
+
+})
+// snack 7 
+
+test('La funzione findPostById restituisce il post corretto dato l’array di post e l’id', () => {
+
+})
